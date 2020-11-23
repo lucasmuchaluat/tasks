34b4b8243d565f756b9fbc1d6f92cdcadbcdd11c
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2020-11-22 22:28:40 -0300

    testing django framework

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
+from .views import TaskCreate, TaskList, TaskDelete, TaskUpdate
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('listar', TaskList.as_view()),
+    path('adicionar', TaskCreate.as_view()),
+    path('atualizar/<int:pk>', TaskUpdate.as_view()),
+    path('deletar/<int:pk>', TaskDelete.as_view())
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+from tasks.models import Task
+from django.core import serializers
+from django.forms.models import model_to_dict
+from rest_framework import generics
+from rest_framework.response import Response
+from tasks.serializer import TaskSerializer
 
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+class TaskCreate(generics.CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskList(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDelete(generics.DestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
